5d564354aa3913b8f5c3355089c9ab17e6f18977
DillonPA99 <56179193+DillonPA99@users.noreply.github.com>
2025-09-21 16:20:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1289,11 +1289,11 @@ if "player_turnovers" not in st.session_state:
 if "last_turnover_event" not in st.session_state:
     st.session_state.last_turnover_event = None
 
-# if "points_off_turnovers" not in st.session_state:
-#    st.session_state.points_off_turnovers = {'home': 0, 'away': 0}
+ if "points_off_turnovers" not in st.session_state:
+    st.session_state.points_off_turnovers = {'home': 0, 'away': 0}
 
-#if "lineup_points_off_turnovers" not in st.session_state:
-#    st.session_state.lineup_points_off_turnovers = defaultdict(int)
+if "lineup_points_off_turnovers" not in st.session_state:
+    st.session_state.lineup_points_off_turnovers = defaultdict(int)
 
 # ------------------------------------------------------------------
 # Helper functions
