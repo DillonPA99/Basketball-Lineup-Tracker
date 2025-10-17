f78321ea200e3968cc248a8ee088c406fe64463e
DillonPA99 <56179193+DillonPA99@users.noreply.github.com>
2025-10-17 15:09:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6326,7 +6326,7 @@ def display_efficiency_comparison():
             st.success(f"**Momentum**\n\n## 🔥 Hot")
             st.caption(f"Recent +{ppp_diff:.2f} better!")
         else:
-            st.error(f"**Momentum**\n\n 📉 Cooling")
+            st.error(f"**Momentum**\n\n# 📉 Cooling")
             st.caption(f"Recent {ppp_diff:.2f} worse")
 
 def display_possession_details():
